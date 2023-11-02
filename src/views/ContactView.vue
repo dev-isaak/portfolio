7805197e31d109dd1460e7083cacbc9cad75b1e9
Isaac Montenegro <isaac.montenegro1992@gmail.com>
2023-11-02 12:46:29 +0100

    WIP

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -7,12 +7,12 @@ import SectionTemplate from '@/templates/SectionTemplate.vue'
 <template>
   <SectionTemplate title="Contact">
     <v-sheet class="bg-transparent ma-auto pa-6" max-width="500">
-        <v-form name="contact" method="POST" data-netlify="true" validate-on="submit lazy" @submit.prevent="submit">
+        <v-form name="contact" method="POST" data-netlify="true">
           <v-text-field label="Your Name"></v-text-field>
           <v-text-field label="Your email*" :rules="rules"></v-text-field>
           <v-textarea label="" no-resize></v-textarea>
           <div class="d-flex justify-center">
-            <v-btn type="submit" variant="outlined" color="teal">Contact</v-btn>
+            <input type="submit" variant="outlined" color="teal" label="Contact" />
           </div>
         </v-form>
     </v-sheet>
