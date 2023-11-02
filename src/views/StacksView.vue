8f46bbdd0e9ca3f8586f55b6cc776c9c0ab36eec
Isaac Montenegro <isaac.montenegro1992@gmail.com>
2023-11-02 15:10:17 +0100

    WIP

--- src/views/StacksView.vue
+++ src/views/StacksView.vue
@@ -1,7 +1,6 @@
 <script setup>
-import MainTitle from '@/components/general/MainTitle.vue'
 import Css3Logo from '/public/logos/css3.png'
-import DockerLogo from '/public/logos/docker.svg'
+import DockerLogo from '/public/logos/docker.png'
 import Html5Logo from '/public/logos/html5.svg'
 import JsLogo from '/public/logos/js.png'
 import NodeLogo from '/public/logos/nodejs.png'
@@ -14,37 +13,37 @@ import SectionTemplate from '@/templates/SectionTemplate.vue'
 <template>
   <SectionTemplate title="Stacks">
   <v-sheet class="d-flex flex-wrap justify-center w-100 bg-transparent" >
-    <v-sheet class="w-100 d-flex flex-wrap justify-space-between bg-transparent" max-width="700">
-      <div class="logo-stack-container">
-        <v-img class="ma-6 stack-image" width="100" :src="Css3Logo"></v-img>
+    <v-sheet class="w-100 d-flex flex-wrap justify-center bg-transparent" max-width="700">
+      <div class="logo-stack-container my-4">
+        <v-img class="mx-10 stack-image" width="50" :src="Css3Logo"></v-img>
         <h3 class="text-logo">CSS 3</h3>
       </div>
-      <div class="logo-stack-container">
-        <v-img class="ma-6 stack-image" width="100" :src="Html5Logo"></v-img>
+      <div class="logo-stack-container my-4">
+        <v-img class="mx-10 stack-image" width="50" :src="Html5Logo"></v-img>
         <h3 class="text-logo">Html 5</h3>
       </div>
-      <div class="logo-stack-container">
-        <v-img class="ma-6 stack-image" width="100" :src="JsLogo"></v-img>
+      <div class="logo-stack-container my-4">
+        <v-img class="mx-10 stack-image" width="50" :src="JsLogo"></v-img>
         <h3 class="text-logo">JavaScript</h3>
       </div>
-      <div class="logo-stack-container">
-        <v-img class="ma-6 stack-image" width="100" :src="VueJsLogo"></v-img>
+      <div class="logo-stack-container my-4">
+        <v-img class="mx-10 stack-image" width="50" :src="VueJsLogo"></v-img>
         <h3 class="text-logo">Vue.js</h3>
       </div>
-      <div class="logo-stack-container">
-        <v-img class="ma-6 stack-image" width="100" :src="Strapi"></v-img>
+      <div class="logo-stack-container my-4">
+        <v-img class="mx-10 stack-image" width="50" :src="Strapi"></v-img>
         <h3 class="text-logo">Strapi</h3>
       </div>
-      <div class="logo-stack-container">
-        <v-img class="ma-6 stack-image" width="100" :src="NodeLogo"></v-img>
+      <div class="logo-stack-container my-4">
+        <v-img class="mx-10 stack-image" width="50" :src="NodeLogo"></v-img>
         <h3 class="text-logo">Node.js</h3>
       </div>
-      <div class="logo-stack-container">
-        <v-img class="ma-6 stack-image" width="100" :src="SqlLogo"></v-img>
+      <div class="logo-stack-container my-4">
+        <v-img class="mx-10 stack-image" width="50" :src="SqlLogo"></v-img>
         <h3 class="text-logo">Sql</h3>
       </div>
-      <div class="logo-stack-container">
-        <v-img class="ma-6 stack-image" width="100" :src="DockerLogo"></v-img>
+      <div class="logo-stack-container my-4">
+        <v-img class="mx-10 stack-image" width="50" :src="DockerLogo"></v-img>
         <h3 class="text-logo">Docker</h3>
       </div>
     </v-sheet>
@@ -58,7 +57,8 @@ import SectionTemplate from '@/templates/SectionTemplate.vue'
 }
 .stack-image {
   z-index: 10;
-  height:100px;
+  width:auto;
+  height:80px;
   filter: brightness(0.5);
   transition: all 500ms;
 }
@@ -68,6 +68,9 @@ import SectionTemplate from '@/templates/SectionTemplate.vue'
 
 .logo-stack-container:hover .text-logo{
   color:white;
+}
+.text-logo{
+  font-size: .8em;
 }
 
 </style>
